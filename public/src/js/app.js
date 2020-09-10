543055749dcf562ae6ed1194d81d77c7a95879b7
Max Lettenberger <maxlettenberger@gmail.com>
2020-09-10 13:59:54 -0500

    Add promises

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -12,3 +12,28 @@ window.addEventListener('beforeinstallprompt', function(event) {
     console.log('beforeinstallprompt fired');
     event.preventDefault();
 });
+
+var promise = new Promise(function(resolve, reject) {
+    setTimeout(function() {
+        //resolve('This is executed once the timer is done');
+        reject({code: 500, message: 'An error occurred!'});
+    }, 3000);
+});
+
+// promise.then(function(text) {
+//     return text;
+// }, function(err) {
+//     console.log(err.code, err.message)
+// }).then(function(newText) {
+//     console.log(newText);
+// })
+
+promise.then(function(text) {
+    return text;
+}).then(function(newText) {
+    console.log(newText);
+}).catch(function(err) {
+    console.log(err.code, err.message);
+})
+
+console.log('This is executed right after setTimeout()');
