43ad5f3f36aed495e47292a09e019700f37bdbff
Max Lettenberger <maxlettenberger@gmail.com>
2020-09-12 17:08:19 -0500

    Consolidate caching with cacheAll

--- public/sw.js
+++ public/sw.js
@@ -4,9 +4,26 @@ self.addEventListener('install', function(event) {
         caches.open('static')
             .then(function(cache) {
                 console.log('[Service Worker] Precaching App Shell')
-                cache.add('/src/js/app.js/')
-                cache.add('/index.html')
-                cache.add('/')
+                // cache.add('/src/js/app.js/')
+                // cache.add('/src/js/feed.js/')
+                // cache.add('/src/js/fetch.js/')  
+                // cache.add('/src/js/promise.js/')
+                // cache.add('/index.html')
+                // cache.add('/')
+                // cache.add('/src/js/material.min.js/')
+                // cache.add('/src/css/app.css')
+                // cache.add('/src/css/feed.js/')
+                cache.addAll([
+                    '/src/js/app.js/',
+                    '/src/js/feed.js/',
+                    '/src/js/fetch.js/',  
+                    '/src/js/promise.js/',
+                    '/index.html',
+                    '/',
+                    '/src/js/material.min.js/',
+                    '/src/css/app.css',
+                    '/src/css/feed.js/'
+                ])
             })
         );
 });
