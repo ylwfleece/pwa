ed327d3cf2b5f40a9a750c8ceb418898f6753885
Max Lettenberger <maxlettenberger@gmail.com>
2020-09-14 14:39:15 -0500

    Optimizing caching

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,10 @@
+var CACHE_STATIC_NAME = 'static-v4';
+var CACHE_DYNAMIC_NAME = 'dynamic-v2';
+
 self.addEventListener('install', function(event) {
     console.log('[Service Worker] Installing Service Worker ...', event);
     event.waitUntil(
-        caches.open('static')
+        caches.open(CACHE_STATIC_NAME)
             .then(function(cache) {
                 console.log('[Service Worker] Precaching App Shell');
                 cache.addAll([
@@ -25,6 +28,17 @@ self.addEventListener('install', function(event) {
 
 self.addEventListener('activate', function(event) {
     console.log('[Service Worker] Activating Service Worker ...', event);
+    event.waitUntil(
+        caches.keys()
+            .then(function(keyList) {
+                return Promise.all(keyList.map(function(key) {
+                    if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
+                        console.log('[Service Worker] Removing old cache.', key);
+                        return caches.delete(key);
+                    }
+                }));
+            })
+    );
     return self.clients.claim();
 });
 
@@ -38,12 +52,15 @@ self.addEventListener('fetch', function(event) {
                 } else {
                     return fetch(event.request)
                         .then(function(res) {
-                            caches.open('dynamic')
+                            caches.open(CACHE_DYNAMIC_NAME)
                                 .then(function(cache) {
                                     cache.put(event.request.url, res.clone())
                                     return res;
                                 })
-                        });
+                        })
+                        .catch(function(err) {
+
+                        })
                 }
             })
             
