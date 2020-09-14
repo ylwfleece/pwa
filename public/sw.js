7a3c23ac7771c54ed4085c9859ce69268598d3ea
Max Lettenberger <maxlettenberger@gmail.com>
2020-09-13 19:47:16 -0500

    Add caching for more files

--- public/sw.js
+++ public/sw.js
@@ -3,7 +3,7 @@ self.addEventListener('install', function(event) {
     event.waitUntil(
         caches.open('static')
             .then(function(cache) {
-                console.log('[Service Worker] Precaching App Shell')
+                console.log('[Service Worker] Precaching App Shell');
                 // cache.add('/src/js/app.js/')
                 // cache.add('/src/js/feed.js/')
                 // cache.add('/src/js/fetch.js/')  
@@ -22,7 +22,11 @@ self.addEventListener('install', function(event) {
                     '/',
                     '/src/js/material.min.js/',
                     '/src/css/app.css',
-                    '/src/css/feed.js/'
+                    '/src/css/feed.js/',
+                    '/src/images/main-image.jpg/',
+                    'https://fonts.googleapis.com/css?family=Roboto:400,700',
+                    'https://fonts.googleapis.com/icon?family=Material+Icons',
+                    'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
                 ])
             })
         );
